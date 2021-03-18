1a3bc1cbe2628ccd3e3ae9fe77c6a3e9a59ad7ef
Mirela <adisserdarevic@gmail.com>
2021-03-18 23:04:59 +0100

    Added file index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
+
+function myNestedFunction(arr) {
+    // Only change code below this line
+    var arr = myNestedArray;
+    // Only change code above this line
+return arr;
+}
+console.log(myNestedFunction());    // Change this line
+module.exports = myNestedFunction;
