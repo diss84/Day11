bbfadaab0103ec3998fa69acebb5a7017da046ed
Mirela <adisserdarevic@gmail.com>
2021-03-19 00:54:16 +0100

    Added file index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,27 +1,32 @@
 // Create a temps array here
-var temps = [ 
-monday = [31, 32, 19, 37],
-tuesday = [29, 27, 55, 36],
-wednesday = [17, 27, 42, 46],
-thursday = [29, 52, 21, 64],
-friday = [91, 27, 31, 61]
+var temps = [
+   [31, 32, 19, 37],
+   [29, 27, 55, 36],
+   [17, 27, 42, 46],
+   [29, 52, 21, 64],
+    [91, 27, 31, 61]
 ];
 
 // End of temps array
 function myArrayFunction(arr) {
-    /*var temps = [...arr];*/
-    averageDayTemp = [29.75, 36.75, 33, 41.5, 52.5];
+    var newTemps = [...arr];
+    var averageDayTemp = [];
     // Only change code below this line
-    for (var i = 0; i<temps.lenght; i++){
-        for(var k = 0; k < temps[i].length; k++){
-        console.log(averageDayTemp[i][k]);
+    var sum = 0;
+    var count = 0;
+    for (var i = 0; i < newTemps; i++){
+        for (j = 0; j < newTemps[i].lenght; i++) {
+            sum += newTemps[i][j];
+            count += 1;
         }
+        var result = sum / count;
+        averageDayTemp.push(result);
+        count = 0;
+        sum = 0;
     }
     
-
     // Only change code above this line
     return averageDayTemp;
 }
-
-console.log(myArrayFunction(temps.push[temps]));
+console.log(myArrayFunction(temps));
 module.exports = myArrayFunction;
