dcb42c5d6c82117d223bc56a764f5dfb8f383cab
Mirela <adisserdarevic@gmail.com>
2021-03-19 01:09:20 +0100

    Added file index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,31 @@
+function multiplyArrayFunction(myArray) {
+    var sum = 0;
+    var product = 0;
+    // Only change code below this line
+    
+        myArray = [
+        [2],
+        [5, 6, 7],
+        [8, 9],
+        [2.5, 2],
+        [0.5, 0.2],
+        [8]
+        [1,2],
+        [3, 4, 5, 6],
+        [7, 8, 9]];
+
+            for (var i=0; i < myArray.lenght; i++) {
+                sum += myArray[i][j]  };
+                return sum;
+        for (var j=0; j < myArray.lenght; j++) {
+            product *= myArray(i)(j)};            
+            return product;               
+
+        }
+        
+    
+
+    // Only change code above this line
+
+console.log(multiplyArrayFunction(myArray));  // Change this line
+module.exports = multiplyArrayFunction;
