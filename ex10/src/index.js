a613b8134e79f621d18108c7ee66cd4e3dae0b5c
Mirela <adisserdarevic@gmail.com>
2021-03-20 22:37:29 +0100

    Added file index.js

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,17 +1,18 @@
 // Only change code below this line
 function mySplice(arr1, arr2, n) {
-    let combinedArrays = arr2.slice();
-    var temp = arr1.reverse();
-    for (let i = 0; i < temp.length; i++) {
-      combinedArrays.splice(n, 0, temp[i]);
-      n++;
-    }
-    return combinedArrays;
+  arr2.slice();
+  var temp = arr1.reverse();
+  for (let i = 0; i < temp.length; i++) {
+    arr2.splice(n, 0, temp[i]);
+    n++;
   }
-  mySplice([1, 2, 3], [4, 5], 1);
-  // Only change code above this line
-  console.log(mySplice([1, 2, 3], [4, 5], 1));
-  console.log(mySplice([1, 2, 3], [4, 5], 2));
-  console.log(mySplice(["b", "c"], ["a", "d"], 1));
-  console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2)); // Change this line
-  module.exports = mySplice;
+  return arr2;
+}
+mySplice([1, 2, 3], [4, 5], 1);
+// Only change code above this line
+console.log(mySplice([1, 2, 3], [4, 5], 1));
+console.log(mySplice([1, 2, 3], [4, 5], 2));
+console.log(mySplice(["b", "c"], ["a", "d"], 1));
+console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2)); // Change this line
+console.log(mySplice([1, 2, 3, 4], [5, 6, 7], 1));
+module.exports = mySplice;
