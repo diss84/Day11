fb89643bf57db01a436c67dcd10d145ecbe0d373
Mirela <adisserdarevic@gmail.com>
2021-03-20 12:23:54 +0100

    Added file index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,31 +1,40 @@
+var myArray1 = [
+    [2],
+    [5, 6, 7],
+    [8, 9]
+];
+var myArray2 = [
+    [2.5, 2],
+    [0.5, 0.2],
+    [8]
+];
+var myArray3 = [
+    [1,2],
+    [3, 4, 5, 6],
+    [7, 8, 9]
+];
+
+
 function multiplyArrayFunction(myArray) {
+    
+    // Only change code below this line   
+    var arr = [];
     var sum = 0;
-    var product = 0;
-    // Only change code below this line
-    
-        myArray = [
-        [2],
-        [5, 6, 7],
-        [8, 9],
-        [2.5, 2],
-        [0.5, 0.2],
-        [8]
-        [1,2],
-        [3, 4, 5, 6],
-        [7, 8, 9]];
-
-            for (var i=0; i < myArray.lenght; i++) {
-                sum += myArray[i][j]  };
-                return sum;
-        for (var j=0; j < myArray.lenght; j++) {
-            product *= myArray(i)(j)};            
-            return product;               
-
+    var pro = 1;
+    for (var i = 0; i < myArray.length; i++) {
+        for (var j = 0; j < myArray[i].length; j++) {
+            pro *= myArray[i][j];
+            sum += myArray[i][j];
         }
-        
-    
+    }
+    arr.push(pro);
+    arr.push(sum);
 
-    // Only change code above this line
+    return arr;
+}
+        // Only change code above this line
 
-console.log(multiplyArrayFunction(myArray));  // Change this line
+console.log(multiplyArrayFunction(myArray1));  // Change this line
+console.log(multiplyArrayFunction(myArray2));  // Change this line
+console.log(multiplyArrayFunction(myArray3));  // Change this line
 module.exports = multiplyArrayFunction;
